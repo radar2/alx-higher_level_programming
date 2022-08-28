5ad17eee22b4ed17c933dfaefd42408af9b12d34
radar2 <securkydroity@gmail.com>
2022-08-28 15:37:43 -0700

    Matrix

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_string_list = list(my_string)
+    count = 0
+    for index in list:
+        if index == 'c' or index == 'C':
+            my_string_list[count] = ""
+        index_count += 1
+    return "".join(my_string_list)
