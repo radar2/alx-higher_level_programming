1a54fed37123707df57371039c5ff1a83e872d16
radar2 <securkydroity@gmail.com>
2022-09-02 17:58:57 -0700

    Fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [item if item != search else replace for item in my_list ]
-
+    new_list = []
+    for item in my_list:
+        if item != search:
+            new_list.append(item)
+        else:
+            new_list.append(replace)
+    return new_list
