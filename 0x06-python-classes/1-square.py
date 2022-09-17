0e649490150377c239c79c5d23c2005334f2d87c
radar2 <securkydroity@gmail.com>
2022-09-17 10:11:05 -0700

    Init Sqyare

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 17 15:53:15 2022
+
+@author: VAS 6
+"""
+
+class Square(object):
+    """
+    Sqaure class that define a square
+    """
+    def __init__(self, size):
+        """ Square init method.
+        
+        Args:
+            size :  The size of a square
+
+        """
+        self.__size = size
