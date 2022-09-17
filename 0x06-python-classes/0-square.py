041ac8d49bcdaaa4c9107798d7fb8fa3e98e82ca
radar2 <securkydroity@gmail.com>
2022-09-17 09:51:55 -0700

    Fix Requirement

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -7,9 +7,5 @@ Created on Sat Sep 17 15:53:15 2022
 """
 
 class Square(object):
-    """ 
-    Square is a class that define a square.
-
-    This class does not import any module
-    """
+    pass
 
