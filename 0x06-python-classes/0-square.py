32b92e26eee10139680ca9189de89f95c687eb68
radar2 <securkydroity@gmail.com>
2022-09-17 09:50:32 -0700

    Fix Requirement

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 17 15:53:15 2022
 
-class Square:
+@author: David Green
+"""
+
+class Square(object):
     """ 
     Square is a class that define a square.
 
