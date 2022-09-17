9ee822d43087ea1db3c82039374f37512c99f3e8
radar2 <securkydroity@gmail.com>
2022-09-17 09:33:41 -0700

    Fix requirement

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -8,6 +8,8 @@ Created on Sat Sep 17 15:53:15 2022
 
 class Square:
     """ 
-    Square is a class that define a square
+    Square is a class that define a square.
+
+    This class does not import any module
     """
 
