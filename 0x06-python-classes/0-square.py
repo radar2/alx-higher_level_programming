f2c477f9513020a989a70557958ea9ff56352d30
radar2 <securkydroity@gmail.com>
2022-09-17 09:53:47 -0700

    Fix Requirement

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -7,5 +7,11 @@ Created on Sat Sep 17 15:53:15 2022
 """
 
 class Square(object):
+    """"
+    Square is a class that define a square
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    """
     pass
 
