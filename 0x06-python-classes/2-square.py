54c3668e4102afe8d63e89cbcd5e08544c9d19dd
radar2 <securkydroity@gmail.com>
2022-09-29 15:28:31 -0700

    All Done

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,25 +2,22 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Sep 17 15:53:15 2022
-
-@author: David Green
 """
 
+
 class Square(object):
+    """Sqaure class that define a square
     """
-    Sqaure class that define a square
-    """
-    def __init__(self, size = 0):
-        """ Square init method.
-        
+
+    def __init__(self, size=0):
+        """Square init method.
         Args:
             size (int):  The size of a square
-        
         Raises:
             TypeError: If size is not an integer
             ValueError: If size is less than 0
-
         """
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,24 +2,21 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Sep 17 15:53:15 2022
-
-@author: David Green
 """
+
 
 class Square(object):
     """
     Sqaure class that define a square
     """
+
     def __init__(self, size = 0):
         """ Square init method.
-        
         Args:
             size (int):  The size of a square
-        
         Raises:
             TypeError: If size is not an integer
             ValueError: If size is less than 0
-
         """
         if type(size) != int:
             raise TypeError("size must be an integer")
@@ -27,7 +24,8 @@ class Square(object):
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-    
+
     def area(self):
         """ return the square of size. """
+
         return self.__size * self.__size
