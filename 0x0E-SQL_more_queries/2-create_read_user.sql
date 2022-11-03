4a38177e2a9511581228f8db49f4d4ac78f018a6
radar2 <securkydroity@gmail.com>
2022-11-03 09:20:14 -0700

    Fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,3 @@
--- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates the database hbtn_0d_2 and the user user_0d_2C
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-ALTER USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
