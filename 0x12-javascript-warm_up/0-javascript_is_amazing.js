9136bc8694dd736d7c67762d8e05b8a718b03ea4
radar2 <securkydroity@gmail.com>
2022-11-28 23:55:12 +0000

    Multiple line console

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//Javascript
+// Javascript
 
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Javascript
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
