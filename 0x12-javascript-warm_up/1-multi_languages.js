7d44037f7273a3201df30500fb31d2097f1b7338
radar2 <securkydroity@gmail.com>
2022-11-29 00:11:29 +0000

    Fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 // Javascript
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const var1 = 'C is fun';
+const var2 = 'Python is cool';
+const var3 = 'JavaScript is amazing';
+console.log(var1);
+console.log(var2);
+console.log(var3);
