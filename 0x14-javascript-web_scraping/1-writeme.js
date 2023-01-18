3e160f1ec3549544042caddc0ed8bc9d786785e0
radar2 <securkydroity@gmail.com>
2023-01-18 00:23:02 +0000

    All done

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// JS Script
+const fs = require('fs');
+const argstr = process.argv[3];
+fs.writeFile(process.argv[2], argstr, function (err, argstr) {
+  if (err) {
+    console.log(err);
+  }
+});
