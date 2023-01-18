8bfef82bbdcc57cb9c88779f60655e89fb825487
radar2 <securkydroity@gmail.com>
2023-01-18 01:07:42 +0000

    Fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const req = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 req(url, function (error, response, body) {
   if (error) { console.log(error); }
   JSON.parse(body).characters.forEach(function (peeps) {
