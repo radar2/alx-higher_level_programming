2f9e70a3e66956ec953713de4be5fbb5735c5c70
radar2 <securkydroity@gmail.com>
2023-05-24 01:04:47 +0000

    Fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // JS Script
-require('request').get(process.argv[2], function (err, r, body) {
+require('request').get(process.argv[2], function (err, body) {
   if (err) {
     console.log(err);
   } else {
