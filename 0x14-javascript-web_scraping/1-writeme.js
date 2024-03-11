9b23fbb5b6cc2bfc4060ee4b50c0fdb069009d9f
radar2 <securkydroity@gmail.com>
2024-03-11 21:39:03 +0000

    done

--- 0x14-javascript-web_scraping/1-writeme.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-// Script that writes a string to a file.
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], function (err, data) {
-  if (err) {
-    console.log(err);
-  }
-});
--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// Script that display the status code of a GET request.
-require('request').get(process.argv[2], function (err, res) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('code: ' + res.statusCode);
-  }
-});
